26b112b62e6f277b655b2d31e2b801aba4b26b42
Brandon Wie <brandonwie.cs@gmail.com>
2021-08-23 02:45:06 +0900

    feat(Sidebar): add Sidebar and create functions to work

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,10 +1,12 @@
+import Sidebar from './Sidebar';
+
 interface DashboardProps {
   id: string;
 }
 const Dashboard: React.FC<DashboardProps> = ({
   id,
 }: DashboardProps): JSX.Element => {
-  return <div>{id}</div>;
+  return <Sidebar id={id} />;
 };
 
 export default Dashboard;
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,9 +1,36 @@
+import { useState } from 'react';
+import { Tab, Nav } from 'react-bootstrap';
+import { SelectCallback } from 'react-bootstrap/esm/helpers';
+
 interface Sidebar {
   id: string;
 }
 
+const CONVERSATION_KEY = 'conversations';
+const CONTACTS_KEY = 'contacts';
+
 const Sidebar: React.FC<Sidebar> = ({ id }: Sidebar) => {
-  return <div>{id}</div>;
+  const [activeKey, setActiveKey] = useState<string>(CONVERSATION_KEY);
+  const handleOnSelect = (eventKey: string | null) => {
+    if (eventKey) {
+      return setActiveKey(eventKey);
+    }
+  };
+
+  return (
+    <div style={{ width: '250px' }} className="d-flex flex-column">
+      <Tab.Container activeKey={activeKey} onSelect={handleOnSelect}>
+        <Nav variant="tabs" className="justify-content-center">
+          <Nav.Item>
+            <Nav.Link eventKey={CONVERSATION_KEY}>Conversations</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Tab.Container>
+    </div>
+  );
 };
 
 export default Sidebar;
